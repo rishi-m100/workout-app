fd167163791616e8ef99a3093d8870492352c3e0
Rishi Mulchandani <rishimulchandani100@gmail.com>
2024-01-03 18:33:43 -0500

    firebase

--- src/App.js
+++ src/App.js
@@ -6,8 +6,33 @@ import Exercises from "./components/exercises";
 import Log from "./components/log";
 import Profile from "./components/profile";
 import NavBar from "./components/navbar";
-// import { firebase } from "./firebase";
-// Initialize Analytics and get a reference to the service
+
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/compat/app";
+import "firebase/compat/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCU0RIxegiB2AZ8gZUVzWGKC0G1mbuLyqY",
+  authDomain: "workout-app-498b1.firebaseapp.com",
+  projectId: "workout-app-498b1",
+  storageBucket: "workout-app-498b1.appspot.com",
+  messagingSenderId: "595596484905",
+  appId: "1:595596484905:web:366b45f9ae5a3fb08f9738",
+  measurementId: "G-YNNNTPNHTY",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const analytics = firebase.analytics();
+
+// export { firebaseConfig as firebase };
+s
+
+
+
 
 class App extends Component {
   state = {
