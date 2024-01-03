e3746fb2b0d5f08b8a08b04e36e16160f56390d5
Rishi Mulchandani <rishimulchandani100@gmail.com>
2024-01-03 17:30:49 -0500

    add

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCU0RIxegiB2AZ8gZUVzWGKC0G1mbuLyqY",
+  authDomain: "workout-app-498b1.firebaseapp.com",
+  projectId: "workout-app-498b1",
+  storageBucket: "workout-app-498b1.appspot.com",
+  messagingSenderId: "595596484905",
+  appId: "1:595596484905:web:366b45f9ae5a3fb08f9738",
+  measurementId: "G-YNNNTPNHTY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
